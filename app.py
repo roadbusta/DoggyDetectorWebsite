247e54bbb208b08c9ea5e5a604a0317a9729eae4
Joseph Gulay <joegulay@gmail.com>
2022-04-24 13:44:47 +1000

    Start to create streamlit interface

--- app.py
+++ app.py
@@ -6,18 +6,22 @@ import requests
 '''
 
 
-'''
-Predicted dog Breed
-'''
+# '''
+# Predicted dog Breed
+# '''
 
 
-url = "https://doggy-detector-2022-image-q34gthac5q-ts.a.run.app/predict?BUCKET_NAME=doggy-detector-2022-bucket-v2&BLOB_NAME=test_images/test"
+# url = "https://doggy-detector-2022-image-q34gthac5q-ts.a.run.app/predict?BUCKET_NAME=doggy-detector-2022-bucket-v2&BLOB_NAME=test_images/test"
 
-response = requests.get(url).json()
+# response = requests.get(url).json()
 
-breed = response
+# breed = response['prediction']
 
 
-st.markdown(f'''
-{breed}
-''')
+# st.markdown(f'''
+# {breed}
+# ''')
+
+image_file = st.file_uploader("Upload Images", type = ["png","jpg","jpeg"])
+if image_file is not None:
+    st.write(type(image_file))
